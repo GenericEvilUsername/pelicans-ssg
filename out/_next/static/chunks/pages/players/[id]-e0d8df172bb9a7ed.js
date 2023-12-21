0121810c7eb7854e98e37f66f7bfe71b199133d8
gitName <a@b.c>
2023-12-21 14:29:17 +0200

    out

---
+++ out/_next/static/chunks/pages/players/[id]-e0d8df172bb9a7ed.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[313],{7435:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/players/[id]",function(){return _(9321)}])},9321:function(n,u,_){"use strict";_.r(u),_.d(u,{__N_SSG:function(){return t}});var t=!0;u.default=()=>"Pelaaja"}},function(n){n.O(0,[774,888,179],function(){return n(n.s=7435)}),_N_E=n.O()}]);
---
+++ out/_next/static/lWeM1Ty0f1K4N1WYyhaf0/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fgames\u002F[id]","\u002Fplayers","\u002Fplayers\u002F[id]","\u002Fseasons","\u002Fseasons\u002F[id]","\u002Fteams","\u002Fteams\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
